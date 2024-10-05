0042e86792d2fbc903c23380e0df297b5826c886
SAMUKISZHSD <samuelallannunesdacunha@gmail.com>
2024-10-05 01:19:04 -0300

    implement H2 data and refactor user API

--- todolist/src/main/java/br/com/sgtp/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/sgtp/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.sgtp.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody; // Corrigido para importar RequestBody
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) { // Alterado para @RequestBody
-         System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) { // Alterado para @RequestBody
+         //System.out.println(userModel.getUsername());
+         var user = this.userRepository.findByUsername(userModel.getUsername());
+
+         if(user != null) {
+            System.out.println("Usuário já existe");
+            return null;
+         }
+
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
 
