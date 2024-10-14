6deb610c3b1c4367421ab69084ea773bb2ebf636
SAMUKISZHSD <samuelallannunesdacunha@gmail.com>
2024-10-14 20:39:51 -0300

    create partial in API updatelTaskUser and refactor name

--- todolist/src/main/java/br/com/sgtp/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/sgtp/todolist/task/TaskController.java
@@ -6,17 +6,17 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sgtp.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -56,9 +56,14 @@ public class TaskController {
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        //taskModel.setIdUser((UUID) idUser);
+        //taskModel.setId(id);
+        return this.taskRepository.save(task);
 
     }
 }
