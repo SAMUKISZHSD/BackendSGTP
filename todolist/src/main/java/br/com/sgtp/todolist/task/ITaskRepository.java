ffbf62adce84245968ff086ddbe1f5f9f3e0b729
SAMUKISZHSD <samuelallannunesdacunha@gmail.com>
2024-10-14 21:17:34 -0300

    refactor API updateTaskUser

--- todolist/src/main/java/br/com/sgtp/todolist/task/ITaskRepository.java
+++ todolist/src/main/java/br/com/sgtp/todolist/task/ITaskRepository.java
@@ -1,10 +1,12 @@
 package br.com.sgtp.todolist.task;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
     List<TaskModel> findByIdUser(UUID idUser);
+    TaskModel findByIdAndIdUser(UUID id, UUID idUser);
 }
